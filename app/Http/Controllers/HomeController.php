cca9119c9827670aad1ac627c83883d6931d2569
Mustafa <startmk2020@gmail.com>
2021-02-09 10:18:25 +0100

    page Home fini

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,9 +2,29 @@
 
 namespace App\Http\Controllers;
 
+
+use App\Models\Home_static;
+use App\Models\Home_1liste;
+use App\Models\Home_2liste;
+
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-    //
+    
+    public function index(){
+
+        $homeStatic =Home_static::all();
+        $homeListe1 =Home_1liste::all();
+        $homeListe2 =Home_2liste::all();
+
+        
+        return view('welcome', compact('homeStatic','homeListe1','homeListe2'));
+
+    }
+
+
+
+
+
 }
